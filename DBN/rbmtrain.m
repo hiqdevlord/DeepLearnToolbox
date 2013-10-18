8fe9c60717b1a7adf776d0f5e95ecc9115cbdbaa
hiqdevlord <hiqdevlord@gmail.com>
2013-10-18 17:42:48 +0200

    added known errors

--- DBN/rbmtrain.m
+++ DBN/rbmtrain.m
@@ -5,51 +5,32 @@ function rbm = rbmtrain(rbm, x, opts)
     
     assert(rem(numbatches, 1) == 0, 'numbatches not integer');
 
-	#checking and initialising some options
-    runUntilThresholdIsReached = opts.numepochs < 1;
-    if(!isfield(opts,"threshold")) opts.threshold = 0.01; endif;
-    if(!isfield(opts,"gibbsSamplingSteps")) opts.gibbsSamplingSteps = 1; endif;
-
-    iterator = 0;
-    oldError = 0; currentError = inf;
-    do
-        oldError = currentError;
-        iterator++;
+    for i = 1 : opts.numepochs
         kk = randperm(m);
-        currentError = 0;
+        err = 0;
         for l = 1 : numbatches
             batch = x(kk((l - 1) * opts.batchsize + 1 : l * opts.batchsize), :);
             
-            visibleActivationsAtBegin = batch;
-            hiddenActivationsAtEnd = hiddenActivationsAtBegin = sigmrnd(repmat(rbm.c', opts.batchsize, 1) + visibleActivationsAtBegin * rbm.W');
-            
-            for sampleStep = 1:opts.gibbsSamplingSteps
-                visibleActivationsAtEnd = sigmrnd(repmat(rbm.b', opts.batchsize, 1) + hiddenActivationsAtEnd * rbm.W);
-                hiddenActivationsAtEnd = sigmrnd(repmat(rbm.c', opts.batchsize, 1) + visibleActivationsAtEnd * rbm.W');
-            end
+            v1 = batch;
+            h1 = sigmrnd(repmat(rbm.c', opts.batchsize, 1) + v1 * rbm.W');
+            v2 = sigmrnd(repmat(rbm.b', opts.batchsize, 1) + h1 * rbm.W);
+            h2 = sigmrnd(repmat(rbm.c', opts.batchsize, 1) + v2 * rbm.W');
 
-            c1 = hiddenActivationsAtBegin' * visibleActivationsAtBegin;
-            c2 = hiddenActivationsAtEnd' * visibleActivationsAtEnd;
+            c1 = h1' * v1;
+            c2 = h2' * v2;
 
-            #Calculate the changes to the parameters. Store them in order to compute a momentum term.
             rbm.vW = rbm.momentum * rbm.vW + rbm.alpha * (c1 - c2)     / opts.batchsize;
-            rbm.vb = rbm.momentum * rbm.vb + rbm.alpha * sum(visibleActivationsAtBegin - visibleActivationsAtEnd)' / opts.batchsize;
-            rbm.vc = rbm.momentum * rbm.vc + rbm.alpha * sum(hiddenActivationsAtBegin - hiddenActivationsAtEnd)' / opts.batchsize;
+            rbm.vb = rbm.momentum * rbm.vb + rbm.alpha * sum(v1 - v2)' / opts.batchsize;
+            rbm.vc = rbm.momentum * rbm.vc + rbm.alpha * sum(h1 - h2)' / opts.batchsize;
 
             rbm.W = rbm.W + rbm.vW;
             rbm.b = rbm.b + rbm.vb;
             rbm.c = rbm.c + rbm.vc;
 
-            currentError = currentError + sum(sum((visibleActivationsAtBegin - visibleActivationsAtEnd) .^ 2)) / opts.batchsize;
+            err = err + sum(sum((v1 - v2) .^ 2)) / opts.batchsize;
         end
         
-        currentError /= numbatches;
-        if(runUntilThresholdIsReached)
-            disp(['epoch ' num2str(iterator) '. Average reconstruction error is: ' num2str(currentError)]);
-        else
-            disp(['epoch ' num2str(iterator) '/' num2str(opts.numepochs)  '. Average reconstruction error is: ' num2str(currentError)]);
-        endif
-    until( runUntilThresholdIsReached && (oldError - currentError < opts.threshold) #Condition for running to threshold
-                                  ||
-          !runUntilThresholdIsReached && (iterator >= opts.numepochs)); #Condition for iterating
+        disp(['epoch ' num2str(i) '/' num2str(opts.numepochs)  '. Average reconstruction error is: ' num2str(err / numbatches)]);
+        
+    end
 end
